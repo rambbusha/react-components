403540c982760ba3400b248f979dbd56cbb04010
Magnus Forsberg <magnus.forsberg@gmail.com>
2018-09-24 14:43:10 +0200

    deleted node modules

--- src/App.js
+++ src/App.js
@@ -9,9 +9,28 @@ import { GbSwitcher } from "./components/gbSwitcher";
 
 class App extends Component {
   render() {
-    return <div>
-      <h2>Hello</h2>
-      </div>;
+    return <GbNavAside 
+    links={
+      [{
+        'txt' : 'news',
+        'icon' : <News myClases='nav-aside-icon gb-icon-medium gb-icon-white-opacity-50'/>
+      },
+      {
+        'txt' : 'widgets',
+        'icon' : <Widgets myClases='nav-aside-icon gb-icon-medium gb-icon-white-opacity-50'/>
+      },
+      {
+        'txt' : 'bookmarks',
+        'icon' : <Bookmarks myClases='nav-aside-icon gb-icon-medium gb-icon-white-opacity-50'/>
+      },
+      {
+        'txt' : 'calendar',
+        'icon' : <Calendar myClases='nav-aside-icon gb-icon-medium gb-icon-white-opacity-50'/>
+      },]
+    }
+    userName='Thomas Schneider'
+    userImageUrl='https://images.unsplash.com/photo-1520466809213-7b9a56adcd45?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=6dd9dc582c677370d110940fda65b992'
+  />
   }
 }
 
